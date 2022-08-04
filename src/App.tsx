9e15f66e13ba1a7999e148dfb4a1d75310f6b1fb
Renan <renan-s-b@hotmail.com>
2022-08-04 17:23:45 -0300

    config botão salvar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import UsuariosPage from './pages/Usuarios';
+import HomePage from './pages/Home';
+
 
 const App = function () {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<HomePage />} />
           <Route path='/usuarios' element={<UsuariosPage />} />
         </Routes>
       </BrowserRouter>
--- src/pages/Usuarios.tsx
+++ src/pages/Usuarios.tsx
@@ -8,10 +8,28 @@ const UsuariosPage = function () {
     Lendo,
     ErroLer,
     Lido,
+    Criar,
+    Salvando,
+    Criado,
+    ErroCriar,
   };
-
+  
+  
   const [estado, setEstado] = useState<Estado>(Estado.Lendo);
   const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+  const botaoNovoUsuarioClicado = function (){
+    setEstado(Estado.Criar)
+  };
+  const botaoSalvarClicado = function(){
+    setEstado(Estado.Salvando)
+    setEstado(Estado.Criado)
+    setEstado(Estado.Criar)
+  }
+  const botaoCarregarClicado = function (){};
+  const botaoCancelarClicado = function (){
+    setEstado(Estado.Lendo)
+    setEstado(Estado.Criar)
+  };
 
   useEffect(function () {
     UsuariosService.lerTodos(
@@ -34,11 +52,11 @@ const UsuariosPage = function () {
       {(estado === Estado.ErroLer) && (
         <>
           <p>ERRO! Não foi possível carregar.</p>
-          <button>Carregar</button>
+          <button onClick={botaoCarregarClicado}>Carregar</button>
         </>
       )}
 
-      {(estado === Estado.Lido) && (
+      {((estado === Estado.Lido) || (estado===Estado.Criar) || (estado===Estado.Salvando)) && (
         <>
           <h1>Usuários</h1>
           <ul>
@@ -46,9 +64,32 @@ const UsuariosPage = function () {
               return <li>{usuario.nome}</li>
             })}
           </ul>
-          <button>Novo</button>
+          <button onClick={botaoNovoUsuarioClicado}>Novo Usuario</button>
         </>
       )}
+                {(estado===Estado.Criado)&&(
+                <p>SUCESSO</p>
+                )}
+
+                {(estado===Estado.ErroCriar)&&(
+                   <p>ERRO</p>
+                )}
+
+              {(estado===Estado.Criar)&&(
+            <form><div>
+              <input placeholder='Nome' />
+              <input placeholder='Login' />
+              <input placeholder='Senha' type='password'/>
+              </div>
+              <div>
+                <button onClick={botaoSalvarClicado}>Salvar</button>
+                <button onClick={botaoCancelarClicado}>Cancelar</button>
+              </div>
+              </form>
+              )}
+              {(estado===Estado.Salvando)&&(
+                <p>Salvando...</p>
+              )}
     </>
   );
 };
